3500cc6f05da19a90313cff16e6d7818cd20e966
FairyScript <h@queb.fun>
2023-11-17 16:52:23 +0800

    chord: husky support

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,21 @@
 module.exports = {
+  root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    "plugin:valtio/recommended",
+    'prettier'
   ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': 'warn',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    //RS custom rules
+    'eqeqeq': ['error', 'smart'],
+    '@typescript-eslint/no-unused-vars': 'warn',
   },
 }
