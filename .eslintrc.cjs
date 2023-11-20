aae87320bbcd63c902231e7bd788e3194b87204b
FairyScript <h@queb.fun>
2023-11-20 14:22:58 +0800

    chord: add eslint-react

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,6 +3,8 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'plugin:@typescript-eslint/recommended',
+    "plugin:react/recommended",
+    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
     "plugin:valtio/recommended",
     'prettier'
@@ -17,5 +19,13 @@ module.exports = {
     //RS custom rules
     'eqeqeq': ['error', 'smart'],
     '@typescript-eslint/no-unused-vars': 'warn',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'react/no-unknown-property': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
 }
