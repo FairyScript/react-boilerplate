d2d9ad32b23f880721d3ee2974eb3ea9cd72f409
FairyScript <h@queb.fun>
2023-11-20 14:21:34 +0800

    chord: dev port use vite env

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,30 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import GitRevision from '@jinixx/vite-plugin-git-revision'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react({
-      jsxImportSource: '@emotion/react',
-      plugins: [['@swc/plugin-emotion', {}]],
-    }),
-    tsconfigPaths({
-      root: '.',
-    }),
-  ],
-  server: {
-    host: true,
-    port: 3000,
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    base: '',
+    plugins: [
+      react({
+        jsxImportSource: '@emotion/react',
+        plugins: [['@swc/plugin-emotion', {}]],
+      }),
+      tsconfigPaths({
+        root: '.',
+      }),
+      GitRevision({
+        branch: true,
+        datetimeCommand:
+          'log -1 --date=format:"%Y-%m-%d %H:%M" --pretty=format:"%ad"',
+      }),
+    ],
+    server: {
+      port: Number(env.VITE_PORT) ?? 3000,
+    },
+  }
 })
