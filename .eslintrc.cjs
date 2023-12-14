c91c2ba71f6f692d702b08b8a1e702f5c8e1c21e
FairyScript <h@queb.fun>
2023-12-14 17:10:32 +0800

    chord: remove unused lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,7 +6,6 @@ module.exports = {
     "plugin:react/recommended",
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
-    "plugin:valtio/recommended",
     'prettier'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
@@ -16,7 +15,7 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
-    //RS custom rules
+    //custom rules
     'eqeqeq': ['error', 'smart'],
     '@typescript-eslint/no-unused-vars': 'warn',
     'react/react-in-jsx-scope': 'off',
