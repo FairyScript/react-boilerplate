b9ade51b48865c82093081ff94743bc325a7848c
FairyScript <h@queb.fun>
2025-03-06 16:45:02 +0800

    rm tanstack router

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,12 +1,8 @@
 import { defineConfig } from '@rsbuild/core'
 import { pluginReact } from '@rsbuild/plugin-react'
-import { TanStackRouterRspack } from '@tanstack/router-plugin/rspack'
 export default defineConfig({
   plugins: [pluginReact()],
   tools: {
-    rspack: {
-      plugins: [TanStackRouterRspack()],
-    },
     swc: {
       jsc: {
         experimental: {
